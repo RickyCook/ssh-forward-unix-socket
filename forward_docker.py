1aa5ed504c8006d562375eed8798b0193b3f6c03
Ricky Cook <rcook@odecee.com.au>
2014-11-20 20:53:05 +1100

    Add options to set owner/mode on socket

--- forward_docker.py
+++ forward_docker.py
@@ -13,13 +13,12 @@ except AttributeError:
 
 PARSER = argparse.ArgumentParser(description="Forward unix sockets over SSH")
 PARSER.add_argument("--local_path", help="override the local path. If not given, mirrors the remote_path")
+PARSER.add_argument("--local_user", help="try to set the user the local socket is owned by")
+PARSER.add_argument("--local_group", help="try to set the group the local socket is owned by")
+PARSER.add_argument("--local_mode", help="try to set the file mode of the local socket")
 PARSER.add_argument("ssh_command", help="command to connect to the remote host. Example: ssh myuser@myhost")
 PARSER.add_argument("remote_path", help="path to the remote socket to forward")
 
-SOCAT_COMMAND = (SOCAT_COMMAND,
-                 'UNIX-LISTEN:{local_path},reuseaddr,fork',
-                 'EXEC:{ssh_command} socat STDIO UNIX-CONNECT\\:{remote_path}',
-                 )
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -27,6 +26,20 @@ def main():
     args = PARSER.parse_args()
     logging.debug(args)
 
+    socat_local_opts = []
+    for opt_name in ('user', 'group', 'mode'):
+        opt_val = getattr(args, 'local_%s' % opt_name)
+        if opt_val:
+            socat_local_opts.append('%s=%s' % (opt_name, opt_val))
+
+    socat_local_opts_suffix = ''.join((
+        ',%s' % opt_string for opt_string in socat_local_opts
+    ))
+
+    real_socat_command = (SOCAT_COMMAND,
+                         'UNIX-LISTEN:{local_path},reuseaddr,fork%s' % socat_local_opts_suffix,
+                         'EXEC:{ssh_command} socat STDIO UNIX-CONNECT\\:{remote_path}',
+                         )
     remote_path = args.remote_path
     local_path = args.local_path or remote_path
     real_socat_command = [
@@ -35,7 +48,7 @@ def main():
             remote_path=remote_path,
             ssh_command=args.ssh_command,
         )
-        for s in SOCAT_COMMAND]
+        for s in real_socat_command]
     logging.debug("Real socat command: %s", real_socat_command)
 
     logging.info("Spawning the socat process")
